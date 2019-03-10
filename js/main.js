821de9856e48ed128edcb36ff7a4add765455ca6
akatsuka <akatsukayuuki@akatsukayuukinoMacBook.local>
2019-03-10 10:15:59 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    newItem: '',
+    todos: []
+  },
+  methods: {
+    addItem: function(event) {
+      // alert();
+      if(this.newItem == '') return
+      var todo = {
+        item: this.newItem,
+        isDone: false
+      }
+      this.todos.push(todo)
+      this.newItem = ''
+    },
+    deleteItem: function(index) {
+        // alert(index);
+        this.todos.splice(index,1)
+    }
+  }
+})
